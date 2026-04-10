e8406827435783d6c3a145f529ac2d42ad7bab61
M0onck <1647359114@qq.com>
2026-04-10 20:48:18 +0800

    chore: adjust config to support ssh remote develop

--- launch.sh
+++ launch.sh
@@ -7,5 +7,10 @@ cd /home/m0onck/traffic-emission-monitor-edge
 # 2. 激活虚拟环境
 source venv/bin/activate
 
-# 3. 启动主程序
+# 3. 配置环境变量
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+export WAYLAND_DISPLAY=wayland-0
+export QT_QPA_PLATFORM=wayland
+
+# 4. 启动主程序
 python app_gui.py
