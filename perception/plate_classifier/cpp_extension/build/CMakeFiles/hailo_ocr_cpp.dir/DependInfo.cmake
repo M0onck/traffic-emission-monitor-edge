599ad31ce46a3f397ffade101fc7e3ab6155f150
M0onck <1647359114@qq.com>
2026-04-19 15:41:31 +0800

    refactor: use cpp ocr

---
+++ perception/plate_classifier/cpp_extension/build/CMakeFiles/hailo_ocr_cpp.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/m0onck/traffic-emission-monitor-edge/perception/plate_classifier/cpp_extension/src/bindings.cpp" "CMakeFiles/hailo_ocr_cpp.dir/src/bindings.cpp.o" "gcc" "CMakeFiles/hailo_ocr_cpp.dir/src/bindings.cpp.o.d"
+  "/home/m0onck/traffic-emission-monitor-edge/perception/plate_classifier/cpp_extension/src/plate_recognizer.cpp" "CMakeFiles/hailo_ocr_cpp.dir/src/plate_recognizer.cpp.o" "gcc" "CMakeFiles/hailo_ocr_cpp.dir/src/plate_recognizer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ perception/plate_classifier/cpp_extension/build/CMakeFiles/hailo_ocr_cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hailo_ocr_cpp.
+# This may be replaced when dependencies are built.
